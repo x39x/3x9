9ce46b43446a3d2c4ac85adb2e2c630e073e3281
Y.S <vv.ys.ma@gmail.com>
2025-03-31 04:00:54 +0800

    Mon Mar 31 04:00:54 CST 2025

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    experimental: {
+        mdxRs: true, // 启用Rust实现的MDX编译器
+    },
+
+    //FIX:  https://github.com/vercel/next.js/issues/76395#issuecomment-2721048784
+    //NOTE: https://github.com/vercel/next.js/issues/77216
+    //      https://github.com/hashicorp/next-mdx-remote/issues/488
+    transpilePackages: ["next-mdx-remote"],
+
+    pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+    images: {
+        loader: "custom",
+        loaderFile: "./lib/imageLoader.ts", // custom img loader
+    },
 };
 
 export default nextConfig;
