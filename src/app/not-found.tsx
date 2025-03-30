519b84a0d4fe88a5883c09b5fe228d14fcad32d8
Y.S <vv.ys.ma@gmail.com>
2025-03-31 04:04:54 +0800

    Mon Mar 31 04:04:54 CST 2025 homepage

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import ContentContainer from "@/components/ContentContainer";
+import NotFound from "@/components/404";
+
+export default function MikuNotFound() {
+    return (
+        <ContentContainer>
+            <NotFound />
+        </ContentContainer>
+    );
+}
