c06e6c92a0c4ca3da16c8ebd4ca6b4bf506fd828
Y.S <vv.ys.ma@gmail.com>
2025-03-31 04:58:20 +0800

    add: support for next sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://x39x.cc",
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+    exclude: ["/39img/*"],
+};
