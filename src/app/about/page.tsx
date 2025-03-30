bc481f66f1ecb58f4a3975348971ff6cd963a7e5
Y.S <vv.ys.ma@gmail.com>
2025-03-31 04:08:18 +0800

    Mon Mar 31 04:08:18 CST 2025 about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,44 @@
+import ContentContainer from "@/components/ContentContainer";
+import Avatar from "@/components/Avatar";
+import Image from "next/image";
+import Link from "next/link";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+
+const FuturaBold = localFont({
+    src: "../../public/FuturaBold.otf",
+    display: "swap",
+});
+
+export default function About() {
+    return (
+        <ContentContainer>
+            <div className="absolute inset-0 -z-10">
+                <Image
+                    src="/bg.jpg"
+                    alt="Background"
+                    fill={true}
+                    style={{ objectFit: "cover" }}
+                />
+            </div>
+
+            {/* blur */}
+            <div className="absolute inset-0  backdrop-blur-lg -z-10"></div>
+
+            <div className="flex flex-col justify-center items-center space-y-3 mt-20 text-white">
+                <Avatar size={150} />
+                <Link
+                    href="https://github.com/x39x"
+                    className={` ml-3 text-5xl  hover:underline underline-offset-9 decoration-3  ${FuturaBold.className.trim()}`}
+                >
+                    Matsu
+                </Link>
+            </div>
+        </ContentContainer>
+    );
+}
+
+export const metadata: Metadata = {
+    title: "About Matsu",
+    description: "About Matsu",
+};
