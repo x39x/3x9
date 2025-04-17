d277d5c5b8d9b2d6e23c087762d4976ab3ce7087
Matsu <matsu3x9@icloud.com>
2025-04-17 18:08:55 +0800

    TIME: Thu Apr 17 18:08:55 CST 2025

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import NavBar from "@/components/NavBar";
 import "@/app/globals.css";
-import "katex/dist/katex.min.css";
 
 export default function RootLayout({
     children,
