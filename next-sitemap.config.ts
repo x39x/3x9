60c7c4d2f6e91b950e34e3632e01926b33be4171
Matsu <matsu3x9@icloud.com>
2025-10-23 03:12:54 +0800

    TIME: 2025-10-22T19:12:54.863Z

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,14 +1,3 @@
-// // next-sitemap.config.js
-// /** @type {import('next-sitemap').IConfig} */
-// const config = {
-//     siteUrl: "https://x39x.cc",
-//     generateRobotsTxt: true,
-//     generateIndexSitemap: false,
-//     exclude: ["/39img/*"],
-//     outDir: "out",
-// };
-//
-// export default config;
 import type { IConfig } from "next-sitemap";
 
 const config: IConfig = {
