140789651522e838f420f44b42b3afe87f302475
Matsu <matsu3x9@icloud.com>
2025-10-31 21:11:23 +0800

    Remove about page background

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,6 @@
 import ContentContainer from "@/components/ContentContainer";
 import Avatar from "@/components/Avatar";
 import Link from "next/link";
-import Image from "next/image";
 import localFont from "next/font/local";
 import { RemoveScroll } from "react-remove-scroll";
 
@@ -14,22 +13,6 @@ const FuturaBold = localFont({
 export default function About() {
     return (
         <RemoveScroll>
-            <div className="absolute inset-0 -z-20 h-screen w-screen">
-                <Image
-                    src="https://img.x39x.cc/large/008IpWfqgy1i68w2wa0q8j335s1s0k0m.jpg"
-                    alt="Background"
-                    fill={true}
-                    sizes="100vw"
-                    style={{ objectFit: "cover" }}
-                    priority
-                />
-            </div>
-
-            {/* blur */}
-            <div
-                className="absolute inset-0  backdrop-blur-lg -z-10 h-screen w-screen"
-                style={{ willChange: "backdrop-filter" }}
-            ></div>
             <ContentContainer>
                 <div className="flex flex-col justify-center items-center space-y-3 mt-20 text-white">
                     <Avatar size={150} />
