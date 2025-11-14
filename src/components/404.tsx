b9a70a288d585e6ecd876c86003d6504d28291ee
Matsu <matsu3x9@icloud.com>
2025-11-14 22:05:34 +0800

    TIME: 2025-11-14T14:05:34.655Z

--- src/components/404.tsx
+++ src/components/404.tsx
@@ -1,22 +1,22 @@
 import Image from "next/image";
 import { Josefin_Sans } from "next/font/google";
 const Josefin_Font = Josefin_Sans({
-    weight: "300",
+    weight: "400",
     subsets: ["latin"],
     style: "italic",
 });
 
 const NotFound = () => {
     return (
-        <div className="flex flex-col items-center justify-center w-full text-center min-h-[calc(100vh-10rem)]">
+        <div className="flex flex-col items-center justify-center w-full text-center min-h-[calc(100vh-10rem)] pb-9">
             <Image
                 src="/404.png"
                 alt="404 Not Found"
                 width={500}
                 height={500}
-                className="lg:w-[34%] md:w-[40%] w-[65%]"
+                className="w-[39%] md:w-[33%]"
             />
-            <h1 className={`mt-9 text-sm ${Josefin_Font.className.trim()}`}>
+            <h1 className={`mt-9 text-sm  ${Josefin_Font.className.trim()}`}>
                 Not found
             </h1>
         </div>
