286a4d14c78c8af980d7114d8ed9254b1da13dfe
Matsu <matsu3x9@icloud.com>
2026-05-22 21:22:43 +0800

    TIME: 2026-05-22T13:22:43.671Z

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,14 +1,9 @@
-"use client";
 import clsx from "clsx";
-import { usePathname } from "next/navigation";
 export default function Loading() {
-    const pathname = `/${usePathname()?.split("/")[1]}`;
     return (
         <div
             className={clsx(
-                pathname == "/about"
-                    ? "bg-[#39C5BB] text-white"
-                    : "bg-white dark:bg-[#141414] text-black dark:text-[#F5F5F7]",
+                "bg-white dark:bg-[#141414] text-black dark:text-[#F5F5F7]",
                 "min-h-screen flex items-center justify-center",
             )}
         ></div>
